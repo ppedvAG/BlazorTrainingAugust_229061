0297414f32c608b3a63e8b62974218262c71ca05
Hannes Preishuber <hannesp@ppedv.de>
2023-08-17 16:16:38 +0200

    todo Httpclient mit filtered Liste

---
+++ BlazorTrainingAugust_229061/Pages/Modul4/ToDoModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorTrainingAugust_229061.Pages.Modul4
+{
+    public class ToDoModel
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}
