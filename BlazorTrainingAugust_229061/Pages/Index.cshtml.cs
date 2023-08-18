1a3afcbf27277771dc21728bb4f16883ee738354
Hannes Preishuber <hannesp@ppedv.de>
2023-08-18 15:55:00 +0200

    JavaScript basics

---
+++ BlazorTrainingAugust_229061/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorTrainingAugust_229061.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
