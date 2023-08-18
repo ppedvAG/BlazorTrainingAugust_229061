4048aac98a17735b59f23bbec469fb7480cf42b0
Hannes Preishuber <hannesp@ppedv.de>
2023-08-18 10:22:59 +0200

    Fieldclass Provider für Validierung Anzeige

---
+++ BlazorTrainingAugust_229061/Pages/Modul5/BoostrapCSSFixFieldCssClassprovider.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorTrainingAugust_229061.Pages.Modul5
+{
+    public class BoostrapCSSFixFieldCssClassprovider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+           var isinvalid= editContext.GetValidationMessages(fieldIdentifier).Any();
+            return isinvalid ? "is-invalid" : "is-valid";
+        }
+    }
+}
