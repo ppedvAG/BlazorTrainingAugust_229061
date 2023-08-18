697498bbf627e95e8671d09a785f25ec839cb5d0
Hannes Preishuber <hannesp@ppedv.de>
2023-08-18 09:43:33 +0200

    Person Poco mit DataAnnotions für Validierung

---
+++ BlazorTrainingAugust_229061/Pages/Modul5/Person.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BlazorTrainingAugust_229061.Pages.Modul5
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Da muss was rein")]
+        [StringLength(15,ErrorMessage ="max 15 zeichen")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Da muss was rein")]
+        [Range(140,190,ErrorMessage ="zwischen 140 und 190cm")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage = "Da muss was rein")]
+        public DateTime GebDat { get; set; }
+    }
+}
